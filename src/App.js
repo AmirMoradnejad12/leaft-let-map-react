74fff1c3a8a2f1613e81adbae99a334477027c66
AmirMoradnejad12 <tomoradnejad@gmail.com>
2023-04-04 22:07:52 +0330

    set Center

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Map from "./components/map";
-import { useState } from "react";
 
 function App() {
-  const [location, SetLocation] = useState(null);
   let center = {
-    lat: 35.69981321289795,
-    lng: 51.33732529299457,
+    lat: 48.8584,
+    lng: 2.2945,
   };
 
   return (
